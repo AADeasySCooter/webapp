b52ec733947ff7bf8f37b3e8e5c0f70ec8ac82a8
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2022-04-25 13:14:09 +0200

    test at home

---
+++ testTraduction.php
@@ -0,0 +1,54 @@
+<?php
+    include "config.php";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $lang['title'] ?></title>
+    <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css">
+    <style type="text/css">
+        .footer {
+            left: 0;
+            position: fixed;
+            bottom: 0;
+            text-align: center;
+            color: white;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#"><?php echo $lang['home'] ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><?php echo $lang['pricing'] ?></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><?php echo $lang['contact'] ?></a>
+            </li>
+        </ul>
+    </nav>
+    <div class="container" style="margin-top: 100px;">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-md-offset-3 text-center">
+                <h1><?php echo $lang['title'] ?></h1>
+                <p>
+                    <?php echo $lang['description'] ?>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer bg-dark">
+        <a href="index.php?lang=en"><?php echo $lang['lang_en'] ?></a>
+        | <a href="index.php?lang=bs"><?php echo $lang['lang_bs'] ?></a>
+    </div>
+</body>
+</html>
