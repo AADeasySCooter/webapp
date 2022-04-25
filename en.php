8740643f54803fbef3720fe5d9772a5d5bebfc8a
Gavin <90148140+AperanoGavin@users.noreply.github.com>
2022-04-25 13:16:53 +0200

    test at home

---
+++ en.php
@@ -0,0 +1,11 @@
+<?php
+	$lang = array(
+		"title" => "My Amazing Website",
+		"home" => "Home",
+		"pricing" => "Pricing",
+		"contact" => "Contact",
+		"description" => "This is explanation about my amazing website!",
+		"lang_en" => "English",
+		"lang_fr" => "Français"
+	);
+?>
