8eb296bbb0508c930920875aa00638fe30f07ddf
Gaveen Aperano Mouloungui <gaveen@mbp-de-gaveen.home>
2022-05-26 15:38:04 +0200

    build a multi fr en 25%

--- index.php
+++ index.php
@@ -131,24 +131,53 @@ include('includes/db.php');
                       
         </div>
     </section>
-    <!-- end of collection -->
-
-    <!-- special products -->
+    
   
     <!-- end of special products -->
 
     <!-- blogs -->
-    <section id = "offers" class = "py-5">
-        <div class = "container">
-            <div class = "row d-flex align-items-center justify-content-center text-center justify-content-lg-start text-lg-start">
-                <div class = "offers-content">
-                    <span class = "text-white">Discount Up To 40%</span>
-                    <h2 class = "mt-2 mb-4 text-white">Grand Sale Offer!</h2>
-                    <a href = "#" class = "btn">Buy Now</a>
-                </div>
+    <section class="vh-40" style="background-color: #4B515D;">
+  <div class="container py-5 h-100">
+
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col-md-8 col-lg-6 col-xl-4">
+
+        <div class="card" style="color: #4B515D; border-radius: 35px;">
+          <div class="card-body p-4">
+
+            <div class="d-flex">
+              <h6 class="flex-grow-1">Warsaw</h6>
+              <h6>15:07</h6>
             </div>
+
+            <div class="d-flex flex-column text-center mt-5 mb-4">
+              <h6 class="display-4 mb-0 font-weight-bold" style="color: #1C2331;"> 13°C </h6>
+              <span class="small" style="color: #868B94">Stormy</span>
+            </div>
+
+            <div class="d-flex align-items-center">
+              <div class="flex-grow-1" style="font-size: 1rem;">
+                <div><i class="fas fa-wind fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 40 km/h
+                  </span></div>
+                <div><i class="fas fa-tint fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 84% </span>
+                </div>
+                <div><i class="fas fa-sun fa-fw" style="color: #868B94;"></i> <span class="ms-1"> 0.2h </span>
+                </div>
+              </div>
+              <div>
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-weather/ilu1.webp"
+                  width="100px">
+              </div>
+            </div>
+
+          </div>
         </div>
-    </section>
+
+      </div>
+    </div>
+
+  </div>
+</section>
     <!-- end of blogs -->
   <!-- blogs -->
  
