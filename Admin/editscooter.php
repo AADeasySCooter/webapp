71d0e2b9f89eb7078f270821d171127c74531675
Gaveen Aperano Mouloungui <gaveen@pc-5-59.customer.ask4.lan>
2022-06-19 19:10:23 +0200

    add trot admin

---
+++ Admin/editscooter.php
@@ -0,0 +1,73 @@
+<?php 
+ include('includes/db.php');
+ include('includes/head.php');
+ include('includes/header.php');
+
+
+
+ ?>
+ <!DOCTYPE html>
+
+ <main class="mt-5 pt-3">
+     <div class="container-fluid">
+         <!--add div here-->
+        
+        <div class="row">
+                <div class="col-md-12 mb-3">
+                    <div class="card">
+                    <div class="card-header">
+                        <span><i class="bi bi-table me-2"></i></span> scooters
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                        <table
+                            id="example"
+                            class="table table-striped data-table"
+                            style="width: 100%"
+                        >
+                            <thead>
+                            <tr>
+                                
+                                <th>Name</th>
+                                <th>status</th>
+                                <th>Code</th>
+                                <th>Actions</th>
+                                
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php   $getscooter = $bdd->query("SELECT * FROM scooter ORDER BY created_at "); 
+                            
+                            while($scooter = $getscooter->fetch()){ ?>
+                            <tr id = "<?php echo $scooter['id']; ?>">
+                            
+
+                                
+                                <td data-target="scooter_name"><?= $scooter['scooter_name'] ;?></td>
+                                <td data-target="scooter_status"><?= $scooter['scooter_status'] ;?></td>
+                                <td data-target="scooter_code">><?= $scooter['scooter_code'] ;?></td>
+                                <td> <a type="submit" class="btn btn-primary" href="updatescooter.php?id=<?=$scooter['id'] ;?>" target="_blank"> UPDATE </td>
+                                <td> <a type="submit" class="btn btn-danger" href="Deletescooter.php?id=<?=$scooter['id'] ;?>" target="_blank"> DELETE </td>
+                               
+                               
+                            
+                            </tr>
+                            <?php
+                                
+                            } ?>
+                            </tbody>
+                            
+                        </table>
+                            
+                                    
+                                    
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+        </div>
+       
+    </div>
+
+    </main>
---
+++ Admin/updatescooter.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+
+<main class="mt-5 pt-3">
+     <div class="container-fluid">
+         <!--add div here-->
+        <div class="row">
+          <div class="col-md-12">
+            <h4>Dashboard</h4>
+
+            <?php
+            include('includes/head.php');
+            include('includes/header.php');
+            include('includes/db.php');
+
+                if(count($_POST)>0)  {
+                    $idd = $_GET['id'];
+                    $scooter_name = $_POST['scooter_name'];
+                    $scooter_status = $_POST['scooter_status'];
+                    $scooter_code = $_POST['scooter_code'];
+                    $scooter_image = $_POST['scooter_image'];
+                    
+          
+                    $radmin = $bdd->query( "UPDATE scooter set scooter_name = '$scooter_name' , scooter_status = '$scooter_status'
+                    ,
+                    scooter_code = '$scooter_code',
+                    scooter_image = '$scooter_image'
+                    WHERE id = '$idd'  " );
+                    $message[] = 'scooter update ';
+
+                }
+                $id = $_GET['id'];
+                $result = $bdd->query( "SELECT * FROM scooter WHERE id = $id " ) ;
+                $response = $result->fetch();
+
+
+ ?>
+          </div>
+        </div>
+        <div class="row">
+                <div class="col-md-12 mb-3">
+                    <div class="card">
+                    <div class="card-header">
+                        <span><i class="bi bi-table me-2"></i></span> users
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                                <form name="frmUser" method="post" action=""> 
+                                <input type="text" name="scooter_name" value="<?= $response['scooter_name'] ;?>">
+                                <br>
+                                <input type="text" name="scooter_status" value="<?= $response['scooter_status'] ;?>">
+                                
+                                <br>
+                                <input type="text" name="scooter_code" value="<?= $response['scooter_code'] ;?>">
+                                <br>
+                                <input type="file" name="scooter_image" value="<?= $response['scooter_image'] ;?>">        
+                                <br>
+                                <input type="submit" class="btn btn-secondary" name="submit" value="submit" class="btn btn">
+                                <a href="editscooter.php" class="btn btn-primary">Scooter</a>
+
+                                </form>
+                          
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+        </div>
+
+      <?php var_dump($response); 
+      ?>
+    </div>
+    </main>
+    
+
+
+
+
+
