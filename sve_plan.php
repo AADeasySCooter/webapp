b9094d3ed53932bffdb3b48b85013715e92f5f27
Gaveen Aperano Mouloungui <gaveen@pc-5-59.customer.ask4.lan>
2022-06-26 13:42:54 +0200

    add first plan

--- sve_plan.php
+++ sve_plan.php
@@ -6,7 +6,7 @@ include('includes/db.php');
 
 //changer le status de l'id récupéré en paramètre de la page en 3
 $id = $_POST['id'];
-$q = " UPDATE  scooter SET  scooter_status=3 WHERE id= $id ";
+$q = " UPDATE  scooter SET  scooter_status=3 , take_at =CURRENT_TIMESTAMP WHERE id= $id ";
 $req = $bdd->prepare($q);
 $req->execute();    
 
