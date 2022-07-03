413be26a753d67be0d17ca4a75e4a7729707201e
Gaveen Aperano Mouloungui <gaveen@MacBook-Pro-de-Gaveen.local>
2022-07-03 18:29:40 +0200

    add leave your scooter with lv.php

--- lv_plan.php
+++ lv_plan.php
@@ -5,4 +5,6 @@ include('includes/db.php');
 
 //update le status de la trot à 1 enlever le user_id et done_at et take_at
 $id = $_POST['id'];
-$qq = " UPDATE  scooter SET  scooter_status=1 , user_id = NULL,done_at =NULL  , take_at =NULL WHERE id= $id ";
+$qq = " UPDATE  scooter SET  scooter_status= 1 , user_id= 0 ,done_at= NULL  , take_at= NULL WHERE id= $id ";
+$req = $bdd->prepare($qq);
+$req->execute();
