a0cbc8396547bb6c4326aedeac8916a8091d7d9a
Gaveen Aperano Mouloungui <gaveen@MacBook-Pro-de-Gaveen.local>
2022-07-14 16:21:50 +0200

    remove twitter

--- index.php
+++ index.php
@@ -140,25 +140,8 @@ include('includes/function.php');
                       
         </div>
     </section>
-    <section>
-      <div class="container">
-            <div class="row align-items-start">
-                        <div class="col">
-                              <blockquote class="twitter-tweet"><p lang="en" dir="ltr">What is that<br>🤨those aren&#39;t your scooters waiting for you there?? <a href="https://t.co/nUO70aHaiD">pic.twitter.com/nUO70aHaiD</a></p>&mdash; ELECTRACKK (@MEB70_) <a href="https://twitter.com/MEB70_/status/1547166862118625280?ref_src=twsrc%5Etfw">July 13, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                        </div>
-            </div>
-      </div>
-
-                 
-    </section>
-    <style>
-       
-        .twitter-tweet {
-            width: 100%;
-            margin: 0 auto;
-        }
-        
-    </style>
+   
+  
     
   
     <!-- end of special products -->
